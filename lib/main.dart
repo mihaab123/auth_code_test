d1d321ab8bc57952d4c2bd84988fa8cb46861286
Defender <miha.ab.2017@gmail.com>
2022-04-24 18:13:09 +0300

    firebase connect

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:auth_code_test/pages/home/home_page.dart';
 import 'package:auth_code_test/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
